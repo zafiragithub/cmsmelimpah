fec5666585787e9d4959445d4a34ba4242ad10b4
zafiragithub <33481431+zafiragithub@users.noreply.github.com>
2026-03-19 13:02:44 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+// Ganti URL ini dengan URL Google Apps Script Anda jika suatu saat Anda deploy ulang
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzR5CgdfZ-1pzFxcK1bZIGWQoHqUnrHNG93D2Rwgw5hgZ4D6GSi7JmjQkjq9k2jlqcl/exec";
